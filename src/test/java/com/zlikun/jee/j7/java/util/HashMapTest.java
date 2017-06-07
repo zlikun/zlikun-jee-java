a262a11633eac39d31074a8ea35a66e05a1b500a
zlikun <zlikun-dev@hotmail.com>
2017-06-07 13:58:06 +0800

    单线程条件下测试扩容对HashMap性能影响

--- src/test/java/com/zlikun/jee/j7/java/util/HashMapTest.java
+++ src/test/java/com/zlikun/jee/j7/java/util/HashMapTest.java
@@ -1,6 +1,7 @@
 package com.zlikun.jee.j7.java.util;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -39,6 +40,67 @@ public class HashMapTest {
         Assert.assertEquals(0.75F ,map._getLoadFactor() ,0.00F);
         // 重新计算阈值
         Assert.assertEquals(6 ,map._getThreshold());
+    }
+
+    /**
+     * 测试HashMap扩容能性能的影响
+     */
+    @Test @Ignore
+    public void resize_performance() {
+
+        int power = 12 ;
+        int loop = 100 * 1000 ;
+
+        // 两次扩容测试
+        long time = System.currentTimeMillis() ;
+
+        for (int i = 0; i < loop; i++) {
+            twice_resize(power);
+        }
+
+        // 2789 / 2759 / 2865
+        System.out.printf("程序执行耗时：%d 毫秒!\n" ,System.currentTimeMillis() - time);
+
+        // 无扩容测试
+        time = System.currentTimeMillis() ;
+
+        for (int i = 0; i < loop; i++) {
+            no_resize(power);
+        }
+
+        // 2466 / 2511 / 2539
+        System.out.printf("程序执行耗时：%d 毫秒!\n" ,System.currentTimeMillis() - time);
+
+    }
+
+    /**
+     * 无扩容测试
+     */
+    private void no_resize(int power) {
+        // 初始容量 1 << 12 ，表示2的12次幂，等价于 (int) Math.pow(2 ,12)，值为：4096
+        HashMap<String ,String> map = new HashMap<>(1 << power) ;
+
+        // 结束时插入未超过阈值，不触发扩容
+        int length = 1 << power * 3 / 4 ;
+        for (int i = 0; i < length; i++) {
+            map.put("key-" + i ,"value-" + i) ;
+        }
+    }
+
+    /**
+     * 两次扩容测试
+     */
+    private void twice_resize(int power) {
+
+        HashMap<String ,String> map = new HashMap<>(1 << (power - 2)) ;
+
+        // 第一次扩容，扩容为 1 << (power - 1)
+        // 第二次扩容，扩容为 1 << power
+        // 结束时插入未超过阈值，不触发扩容
+        int length = 1 << power * 3 / 4 ;
+        for (int i = 0; i < length; i++) {
+            map.put("key-" + i ,"value-" + i) ;
+        }
     }
 
 }
